423f20f63bc2bd31ee2a76ef1499b6d1df7a1c76
Sigurdur Jakobsson <sigurdurj09@ru.is>
2024-03-17 19:02:29 +0000

    Final defense comment updates

--- cameraCalibration/src/imageManipulation.cpp
+++ cameraCalibration/src/imageManipulation.cpp
@@ -10,9 +10,9 @@
 
 //Image macros
 #define ALPHA 12.0 //Contrast // Blinds -> 10.0 / Daylight -> 8.0 / Main lab testing -> 8.0
-#define BETA -100 //Brightness / Main lab testing -> -100
-#define BRIGHTTHRESHOLD 20  //Blinds -> 22 / Daylight -> 180 / Main lab testing -> 30
-#define BLACKTHRESHOLD 60 //Threshold for black mask //Blinds -> 50 / Daylight -> 200 / Main lab testing -> 60
+#define BETA -150 //Brightness / Main lab testing -> -100
+#define BRIGHTTHRESHOLD 30  //Blinds -> 22 / Daylight -> 180 / Main lab testing -> 30
+#define BLACKTHRESHOLD 50 //Threshold for black mask //Blinds -> 50 / Daylight -> 200 / Main lab testing -> 60
 //Hough Macros
 #define HOUGHMINRAD 4 // Main lab testing -> 4
 #define HOUGHMAXRAD 7 // Main lab testing -> 7
